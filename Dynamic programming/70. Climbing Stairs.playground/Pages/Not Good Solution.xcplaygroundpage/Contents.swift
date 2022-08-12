9df4c34b44ab4429112306489f765ffe1f74e559
Seven Tsai <mvpscottjon@gmail.com>
2022-08-12 14:29:15 +0800

    update 746. Min Cost Climbing Stairs

--- Dynamic programming/70. Climbing Stairs.playground/Pages/Not Good Solution.xcplaygroundpage/Contents.swift
+++ Dynamic programming/70. Climbing Stairs.playground/Pages/Not Good Solution.xcplaygroundpage/Contents.swift
@@ -12,18 +12,39 @@ class Solution {
     func climbStairs(_ n: Int) -> Int {
         return climb(i: 0, n: n)
     }
-    
+
     func climb(i: Int, n: Int) -> Int {
         if i > n {
             return 0
         }
-        
+
         if i == n {
             return 1
         }
-        
+
         return climb(i: i + 1, n: n) + climb(i: i + 2, n: n)
+    }
+    
+    // equal to
+    func climbStairs2(_ n: Int) -> Int {
+        return climb2(i: n, n: n)
+    }
+    
+    func climb2(i: Int, n: Int) -> Int {
+        if i < 0 {
+            return 0
+        }
+        
+        if i == 0 {
+            return 1
+        }
+        
+        return climb2(i: i - 1, n: n) + climb2(i: i - 2, n: n)
     }
 }
 
-Solution().climbStairs(35)
+let stairs = 9
+//Solution().climbStairs(stairs)
+//Solution().climbStairs2(stairs)
+print(Solution().climbStairs(stairs) == Solution().climbStairs2(stairs))
+
